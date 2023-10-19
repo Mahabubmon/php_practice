94a405926439bfac0e8cafc595ddc76a16bdcbdc
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-19 17:44:13 +0600

    global scope

--- classes.php
+++ classes.php
@@ -6,5 +6,12 @@
       $z = $x + $y;  
       echo "The sum of $x and $y is =  $z";
 
+       //Variable with global scope
+    $a= 9;//global scope
+    function mytest(){
+      // using x inside this function will generate an error
+      echo "<p>Variable a inside function is: $a</p>";
+      echo "<br>";
 
+    }
   ?>
