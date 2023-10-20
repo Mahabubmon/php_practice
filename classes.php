de9b238e3d9945dbb8c8fed72ec3817ed5cc1b52
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-20 21:23:10 +0600

    else_if

--- classes.php
+++ classes.php
@@ -164,5 +164,16 @@ if($x >"10"){
   
 }
 
+$t = date("H");
+
+if ($t < "10") {
+  echo "Have a good morning!";
+} elseif ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+
+
 
   ?>
