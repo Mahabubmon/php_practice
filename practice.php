2839a353f78c6c4357e33d50780df4446b0e6614
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-22 20:03:10 +0600

    form_practice

--- practice.php
+++ practice.php
@@ -320,7 +320,7 @@ for($row = 0; $row < 4; $row++){
 }
   ?>
 
-<html>
+<!-- <html>
      <body>
 
      <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
@@ -328,17 +328,30 @@ for($row = 0; $row < 4; $row++){
        <input type="submit">
      </form>
 
-     <?php
-     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   // <?php
+     //if ($_SERVER["REQUEST_METHOD"] == "POST") {
        // collect value of input field
-       $name = $_REQUEST['fname'];
-       if (empty($name)) {
-         echo "Name is empty";
-       } else {
-         echo $name;
-       }
-     }
-     ?>
+      // $name = $_REQUEST['fname'];
+       //if (empty($name)) {
+         //echo "Name is empty";
+       //} else {
+        // echo $name;
+       //}
+     //} -->
+     ?> -->
 
-     </body>
-     </html>
+     //</body>
+     //</html>
+
+//form     
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
