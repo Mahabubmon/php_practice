5a2ed6f21034576ec7d5ed4a37d74205835f1e4e
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-22 19:30:48 +0600

    super-global

--- practice.php
+++ practice.php
@@ -319,3 +319,26 @@ for($row = 0; $row < 4; $row++){
   echo "</ul>";
 }
   ?>
+
+<html>
+     <body>
+
+     <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+       Name: <input type="text" name="fname">
+       <input type="submit">
+     </form>
+
+     <?php
+     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+       // collect value of input field
+       $name = $_REQUEST['fname'];
+       if (empty($name)) {
+         echo "Name is empty";
+       } else {
+         echo $name;
+       }
+     }
+     ?>
+
+     </body>
+     </html>
