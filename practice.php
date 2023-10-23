ea2d59f943a943420ab34c021942b89b99e953bc
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-23 09:34:24 +0600

    validation_preg_match

--- practice.php
+++ practice.php
@@ -434,4 +434,11 @@ Gender:
 <input type="submit" name="submit" value="Submit">
 
 </form>
+
+
+$name = test_input($_POST["name"]);
+if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+  $nameErr = "Only letters and white space allowed";
+}
 ?>
+
