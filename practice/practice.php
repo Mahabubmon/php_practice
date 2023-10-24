d64586cd8b429b5b5388bcba015b125c5d9304cb
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-24 09:03:57 +0600

    f_file

--- practice/practice.php
+++ practice/practice.php
@@ -549,3 +549,10 @@ echo date("Y-m-d h:i:sa", $d) . "<br>";
 $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
 echo fgets($myfile);
 fclose($myfile);
+
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+// Output one line until end-of-file
+while(!feof($myfile)) {
+  echo fgets($myfile) . "<br>";
+}
+fclose($myfile);
