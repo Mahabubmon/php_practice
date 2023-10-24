740cc1df2f15021d3e1f8a0d278f1f9472133779
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-24 11:08:59 +0600

    accessing_file

--- practice/practice.php
+++ practice/practice.php
@@ -556,3 +556,12 @@ while(!feof($myfile)) {
   echo fgets($myfile) . "<br>";
 }
 fclose($myfile);
+
+
+//accessing a file
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
