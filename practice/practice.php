325e8743a018e829b659f6e46988846e6b3390d7
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-24 08:32:30 +0600

    time_fortmate

--- practice/practice.php
+++ practice/practice.php
@@ -527,3 +527,6 @@ echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
+
+//time formate
+echo "The time is " . date("h:i:sa");
