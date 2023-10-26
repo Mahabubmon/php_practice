b2178764211338213402f59d5fb41e1a2a39a6d2
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-26 15:40:16 +0600

    array_ascort

--- practice/practice.php
+++ practice/practice.php
@@ -838,3 +838,11 @@ $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
 
 echo json_encode($age);
 ?>
+
+
+<?php 
+$coder = aray ("Rahul","Arif","Tamim");
+next($coder);
+echo "The current position key is:".key($coder);
+
+?>
