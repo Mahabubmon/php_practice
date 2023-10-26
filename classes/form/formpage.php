77bff76d70d21ea5733302814cd0bc3387bbc812
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-26 17:12:55 +0600

    form_radio

--- classes/form/formpage.php
+++ classes/form/formpage.php
@@ -7,6 +7,14 @@
     }
 </script>
 
+<?php 
+if(isset($_POST['gender'])){
+    $gen = $_POST['gender'];
+    echo $gen;
+}
+
+?>
+
 <div id="output"></div>
 <form action="" method="post" name="myform" id="myform" onsubmit="formFunction();">
 <table>
@@ -18,6 +26,15 @@
         <td>Username</td>
         <td><input type="submit" Value="Submit"></td>
         <td><input type="reset" Value="Reset"></td>
+    </tr>
+    <tr>
+        <td>Gender</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Gender</td>
+        <td><input type="radio" name="gender" Value="male">Male</td>
+        <td><input type="radio" name="gender" Value="female">Female</td>
     </tr>
 </table>
 
