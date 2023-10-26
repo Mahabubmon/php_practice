01c09c47dd75681ed834da617c18e768847d0765
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-26 09:00:58 +0600

    filter

--- practice/practice.php
+++ practice/practice.php
@@ -783,3 +783,15 @@ $str = "<h1>Hello World!</h1>";
 $newstr = filter_var($str, FILTER_SANITIZE_STRING);
 echo $newstr;
 ?>
+
+<?php
+$int = 122;
+$min = 1;
+$max = 200;
+
+if (filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max))) === false) {
+  echo("Variable value is not within the legal range");
+} else {
+  echo("Variable value is within the legal range");
+}
+?>
