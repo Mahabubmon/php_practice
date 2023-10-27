7d435de1b3239241b454e61bdb6f376c2262db18
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-27 11:35:25 +0600

    exception

--- practice/practice.php
+++ practice/practice.php
@@ -857,4 +857,19 @@ foreach($coder as $key=>$value){
   echo "Name: ".$key.", Age:".$value."<br>";
 }
 
+<?php
+function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero");
+  }
+  return $dividend / $divisor;
+}
+
+echo divide(5, 0);
+?>
+
+
+
+
+
 ?>
