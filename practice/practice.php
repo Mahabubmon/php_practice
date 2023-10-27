2594cf5162914adaffe9e5d1f39790664de1c148
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-27 11:36:54 +0600

    exception_complete

--- practice/practice.php
+++ practice/practice.php
@@ -891,3 +891,20 @@ try {
   echo "Unable to divide.";
 }
 ?>
+
+<?php
+function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero");
+  }
+  return $dividend / $divisor;
+}
+
+try {
+  echo divide(5, 0);
+} catch(Exception $e) {
+  echo "Unable to divide. ";
+} finally {
+  echo "Process complete.";
+}
+?>
