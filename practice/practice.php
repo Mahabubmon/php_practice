40eaac17bb409c8bf38c152d672a9c8beb1753da
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-28 18:29:17 +0600

    const_1

--- practice/practice.php
+++ practice/practice.php
@@ -1239,3 +1239,15 @@ class Goodbye
 echo Goodbye::LEAVING_MESSAGE;
 
 ?>
+
+<?php
+class Goodbye {
+  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+  public function byebye() {
+    echo self::LEAVING_MESSAGE;
+  }
+}
+
+$goodbye = new Goodbye();
+$goodbye->byebye();
+?>
