1de96e4a3ae66fce32cfdacb31fefd0649eabe16
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-28 17:52:57 +0600

    destruct

--- practice/practice.php
+++ practice/practice.php
@@ -1063,3 +1063,20 @@ echo $apple->get_name();
 echo "<br>";
 echo $banana->get_name();
 ?>
+
+//destruct
+<?php
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name) {
+    $this->name = $name;
+  }
+  function __destruct() {
+    echo "The fruit is {$this->name}.";
+  }
+}
+
+$apple = new Fruit("Apple");
+?>
