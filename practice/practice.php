d009bfc68134653ac2d62e040d89335e2d2030db
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-28 17:50:27 +0600

    construct_1

--- practice/practice.php
+++ practice/practice.php
@@ -1039,3 +1039,27 @@ class Fruit
 $apple = new Fruit("Apple");
 echo $apple->get_name();
 ?>
+<?php
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+}
+
+$apple = new Fruit();
+$banana = new Fruit();
+$apple->set_name('Apple');
+$banana->set_name('Banana');
+
+echo $apple->get_name();
+echo "<br>";
+echo $banana->get_name();
+?>
