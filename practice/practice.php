ace0c3dbbba5a0b42ce97ca3dcd93e985bbd1110
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-28 18:46:53 +0600

    const_2

--- practice/practice.php
+++ practice/practice.php
@@ -1251,3 +1251,50 @@ class Goodbye {
 $goodbye = new Goodbye();
 $goodbye->byebye();
 ?>
+
+
+//abstruct class
+
+<?php 
+//Parent class
+
+abstract class Car{
+  public $name;
+  public function __construct($name){
+    $this->name = $name;
+  }
+  abstract public function intro(): string;
+}
+
+//Childe classes
+class Audi extends Car{
+  public function intro() : string{
+    return "Chose German quality! I'm an $this->name";
+  }
+}
+
+class Volvo extends Car{
+  public function intro(): string{
+    return "Proud  to be Muslim! I'm a $this->name!"
+  }
+}
+class Citroen extends Car{
+  public function intro(): string{
+    return "French extravagance! I'm a $this->name!";
+  }
+}
+
+//creation object
+$audi = new audi("Audi");
+echo $audi->intro();
+echo "<br>";
+
+$volvo =new volvo ("Volvo");
+echo $volvo->intro();
+echo "<br>";
+
+$citroen = new citroen("Citroen");
+echo $citroen->intro();
+
+
+?>
