c82013742dcccdd431209a5bdc0d66796a2d60b7
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-28 18:02:21 +0600

    modifier_ex

--- practice/practice.php
+++ practice/practice.php
@@ -1097,3 +1097,26 @@ $mango->name = 'Mango';
 $mango->color = 'Yellow';//error
 $weight->weight = '300';//error
 ?>
+
+<?php
+class Fruit {
+  public $name;
+  public $color;
+  public $weight;
+
+  function set_name($n) {  // a public function (default)
+    $this->name = $n;
+  }
+  protected function set_color($n) { // a protected function
+    $this->color = $n;
+  }
+  private function set_weight($n) { // a private function
+    $this->weight = $n;
+  }
+}
+
+$mango = new Fruit();
+$mango->set_name('Mango'); // OK
+$mango->set_color('Yellow'); // ERROR
+$mango->set_weight('300'); // ERROR
+?>
