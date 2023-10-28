d7a3123d5bf7081c5d6778d9b41cf97229189784
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-28 18:23:35 +0600

    inheritence_overriding

--- practice/practice.php
+++ practice/practice.php
@@ -1196,3 +1196,33 @@ class Strawberry extends Fruit {
 $strawberry = new Strawberry("Strawberry", "red"); // OK. __construct() is public
 $strawberry->message(); // OK. message() is public and it calls intro() (which is protected) from within the derived class
 ?>
+
+//overriding
+<?php
+class Fruit {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  public function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
+}
+
+class Strawberry extends Fruit {
+  public $weight;
+  public function __construct($name, $color, $weight) {
+    $this->name = $name;
+    $this->color = $color;
+    $this->weight = $weight;
+  }
+  public function intro() {
+    echo "The fruit is {$this->name}, the color is {$this->color}, and the weight is {$this->weight} gram.";
+  }
+}
+
+$strawberry = new Strawberry("Strawberry", "red", 50);
+$strawberry->intro();
+?>
