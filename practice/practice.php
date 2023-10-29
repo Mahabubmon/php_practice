26309e536c32e52b81ac7e5981fd14224ee571c1
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-29 09:05:51 +0600

    list_of_class

--- practice/practice.php
+++ practice/practice.php
@@ -1338,3 +1338,38 @@ $animal = new Cat();
 $animal->makeSound();
 
 ?>
+
+
+<?php 
+
+//Interface definition
+interface Animal{
+  public function makeSound(){
+    echo "Mewo";
+  }
+}
+class Dog implements Animal
+{
+  public function makeSound(){
+    echo "Bark";
+  }
+}
+class Mouse implements Animal
+{
+  public function makeSound(){
+    echo " Squeak ";
+  }
+}
+
+//create a list of animals
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+$animals = array($cat, $dog, $mouse);
+
+//tel the animal to make a sound
+foreach($animals as $animal){
+  $animal->makeSound();
+}
+
+?>
