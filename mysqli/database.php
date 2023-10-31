495d8f8c3348eb00f530a567ff1d276f779dc2b6
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-31 09:10:05 +0600

    PDO_connection

--- mysqli/database.php
+++ mysqli/database.php
@@ -28,8 +28,22 @@ if(!$con){
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+?>
 
+<?php 
+//pdo
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=mydb",
+    $username,$password);
 
-
+    $conn->setattribute(PDO::ATTR_ERRMODE,
+    PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}catch(PDOExpection $e){
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
