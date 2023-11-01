bc279c86059ef4f5a9f0cd3c8b4c494c88fbf1b9
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-01 09:34:53 +0600

    insert multiple

--- mysqli/database.php
+++ mysqli/database.php
@@ -156,3 +156,40 @@ if(mysqli_query($conn, $sql)){
 
 mysqli_close($conn);
 ?>
+
+<?php 
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDBPDO";
+
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
+
+    //set the PDO error mode to exception
+
+    $conn->setAttribute(PDO::attr_ERRMODE,
+    PDO::ERRMODE_EXCEPTION);
+    
+    //begin the the transaction
+    $conn->beginTransaction();
+    //our SQL statements
+    $conn->exec("INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES ('John', 'Doe', 'john@example.com')");
+  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES ('Mary', 'Moe', 'mary@example.com')");
+  $conn->exec("INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES ('Julie', 'Dooley', 'julie@example.com')");
+
+  //commit the transaction
+  $conn->commit();
+  echo "New records created successfully";
+}catch(PDOException $e){
+    //roll back transaction if someting failed
+    $conn->rollback();
+    echo "Error:" . $e->getMessage();
+}
+
+$conn = null;
+
+?>
