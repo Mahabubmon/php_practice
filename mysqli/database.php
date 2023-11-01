e241addd6ffba789fe05eb7dfaa200bd2db8bbe5
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-01 09:24:15 +0600

    insert with last_id

--- mysqli/database.php
+++ mysqli/database.php
@@ -122,7 +122,9 @@ $sql = "INSER INTO MyGuests(firstname, lastname, email)
 VALUES('John', 'Doe', 'john@example.com')";
 
 if($conn->query($sql)=== TRUE){
-    echo "New record created successfully";
+    $last_id = $conn->insert_id;
+    echo "New record created successfully". $last_id;
+    
 }else{
     echo "error:" .sql . "<br>" .$conn->error;
 }
@@ -146,7 +148,8 @@ $sql = "INSERT INTO MyGuests(firstname,lastname,email)
 VALUES('John', 'Doe', 'john@examlpe.com')";
 
 if(mysqli_query($conn, $sql)){
-    echo "New record created successfully";
+    $last_id = mysqli_insert_id($conn);
+  echo "New record created successfully. Last inserted ID is: " . $last_id;
 }else{
     echo "Error: " .$sql . "<br>" .mysqli_error($conn);
 }
