16c614c51ee6122fe454e9ebb256d7b6af6cd286
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-02 22:21:01 +0600

    where

--- mysqli/database.php
+++ mysqli/database.php
@@ -287,3 +287,33 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+
+<?php 
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+//create connection
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+//check connection
+if($conn->connect_error){
+    die("Connection failed: " .$conn->connect_error);
+
+}
+
+$sql = "SELECT id, firstname, lasname, FROM MyGuests Where lastname='Doe";
+
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
+  ?>
+?>
