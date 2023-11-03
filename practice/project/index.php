2880eb6ed8d14ce63fe9ef0299683d1a38260ff6
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-03 22:26:35 +0600

    project

---
+++ practice/project/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo-List</title>
+</head>
+<body>
+    <div class="main-section">
+        <div class="add-section">
+            <form action="">
+                <input type="text"
+                name="tiitle"
+                placeholder="this field is required">
+                <button type="submit">Add &nbsp;<span>&#43;</span></button>
+            </form>
+        </div>
+        <div class="show-todo-section">
+            <input type="checkbox">
+            <h2>This is _____ _____</h2>
+            <small>created:11/03/2023</small>
+        </div>
+    </div>
+    
+</body>
+</html>
