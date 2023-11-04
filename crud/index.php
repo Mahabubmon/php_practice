404f51ab378bd834e8e3e1ed4943b2e3f3d68dc1
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-04 22:57:25 +0600

    connection__

--- crud/index.php
+++ crud/index.php
@@ -2,8 +2,17 @@
 
 
 try{
-    new PDO("mysql:host=127.0.0.1;dbname=crud",'root','');
+   $pdo = new PDO("mysql:host=127.0.0.1;dbname=crud",'root','');
 
 }catch(\Throwable $th){
     die($th->getMessage());
 }
+
+$query ="CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usernaem VARCHAR(255)NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255)NOT NULL,
+    thumbnail VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
