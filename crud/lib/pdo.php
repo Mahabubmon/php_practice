6378a82a5cf3d7f1e529d0e3c61b52ac2d9004aa
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-04 23:09:01 +0600

    modification

--- crud/lib/pdo.php
+++ crud/lib/pdo.php
@@ -81,3 +81,16 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+
+
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+  }
