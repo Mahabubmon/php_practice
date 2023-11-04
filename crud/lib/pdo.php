f83c93131ded37ac784fee4cbb26854979726790
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-04 23:09:22 +0600

    modification

--- crud/lib/pdo.php
+++ crud/lib/pdo.php
@@ -94,3 +94,36 @@ class TableRows extends RecursiveIteratorIterator {
   function current() {
     return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
   }
+
+
+  function beginChildren() {
+    echo "<tr>";
+  }
+
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDBPDO";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests ORDER BY lastname");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
