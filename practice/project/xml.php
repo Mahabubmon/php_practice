f7a8d742e41260f9daf0bc1f5668b200a24b287a
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-05 10:30:31 +0600

    xml_DOM

--- practice/project/xml.php
+++ practice/project/xml.php
@@ -6,3 +6,14 @@ $xmlDoc->load("note.xml");
 
 print $xmlDoc->saveXML();
 ?>
+
+<?php 
+$xmlDoc = new DOMDocument();
+$xmlDoc->load("note.xml");
+
+$x = $xmlDoc->documentElement;
+foreach($x->childNodes AS $item){
+    print $item->nodeName . " = " . $item->nodeValue . "<br>";
+}
+
+?>
