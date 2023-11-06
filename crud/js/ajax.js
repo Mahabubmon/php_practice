5a2e033e391afe8a5a991397e11390f7b972ac54
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-06 10:29:55 +0600

    file_added

--- crud/js/ajax.js
+++ crud/js/ajax.js
@@ -1,1 +1,18 @@
 console.log("Hello");
+
+let fetchBtn = document.getElementById("fetchBtn");
+fetchBtn.addEventListener("click", buttonClickHandler);
+
+function buttonClickHandler() {
+  //instantiate an xhr object
+  const xhr = new XMLHttpRequest();
+
+  //Open the object
+  xhr.open("GET", "mahabub.txt", true);
+
+  //what to do when the respons is ready
+  xhr.onload = function () {
+    console.log(this.responseText);
+  };
+  xhr.send();
+}
