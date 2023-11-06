d3d224f96bd38e4179ad1e7b65cbfec50433a5f0
Mahabubur Rahman <mahabubmon@gmail.com>
2023-11-06 11:13:34 +0600

    json_fetching

--- crud/js/ajax.js
+++ crud/js/ajax.js
@@ -1,5 +1,5 @@
 console.log("Hello");
-
+//https://dummy.restapiexample.com/api/v1/employees
 let fetchBtn = document.getElementById("fetchBtn");
 fetchBtn.addEventListener("click", buttonClickHandler);
 
@@ -11,15 +11,41 @@ function buttonClickHandler() {
   //   xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
   //post requist
   xhr.open("POST", "https://dummy.restapiexample.com/api/v1/create", true);
-  xhr.getAllResponseHeaders(
-    "Content-type",
-    "appilication/x-www-form-urlencoded"
-  );
+  xhr.getAllResponseHeaders("Content-type", "appilication/json");
   //what to do when the respons is ready
   xhr.onload = function () {
-    console.log(this.responseText);
+    if (this.status === 200) {
+      console.log(this.responseText);
+    } else {
+      console.log("some error occured");
+    }
   };
   //send the request
-  params = "name=test&salary=123&age=23";
+  params = `{ name: "test", salary: "123", age: "23" }`;
   xhr.send(params);
+}
+
+let popBtn = document.getElementById("popBtn");
+popBtn.addEventListener("click", popHandler);
+
+function popHandler() {
+  console.log("you have click the pophandler");
+  const xhr = new XMLHttpRequest();
+
+  //Open the object
+  xhr.open("GET", "https://dummy.restapiexample.com/api/v1/employees", true);
+
+  //what to do when the respons is ready
+  xhr.onload = function () {
+    if (this.status === 200) {
+      let obj = JSON.parse(this.responseText);
+      console.log(obj);
+    } else {
+      ("some error occured");
+    }
+  };
+  //send the request
+
+  xhr.send();
+  console.log("we are done fetching employee");
 }
