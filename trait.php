ff45ce027601d800579dc7d1f3f5f2cf55db73f7
Mahabubur Rahman <mahabubmon@gmail.com>
2023-12-28 17:15:11 +0600

    trait_using

---
+++ trait.php
@@ -0,0 +1,21 @@
+<?php
+
+trait msg1
+{
+    public function msg1()
+    {
+        echo "OOP reduces code duplication!";
+    }
+}
+
+trait msg2
+{
+    public function msg2()
+    {
+        echo "These's th msg 2";
+    }
+}
+
+
+
+?>
